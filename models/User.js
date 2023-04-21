d0b5aecef218cad93e1680259b09121d14643559
MatiasTejerina07 <127890622+MatiasTejerina07@users.noreply.github.com>
2023-04-21 18:02:42 -0300

    base de datos (#7)

--- models/User.js
+++ models/User.js
@@ -5,12 +5,12 @@ let schema = new mongoose.Schema({
     password: {type: String, required: true},
     photo:{type: String, required:true},
     role:{type:Number, required:true},
-    is_online:{type:Boolean, required},
+    is_online:{type:Boolean, required: true},
     is_verified:{type:Boolean, required:true},
     verify_code:{type:String, required:true}
 },{
     timestamps: true
 })
 let collection = 'users'
-let User = mongoose.model(schema, collection)
+let User = mongoose.model(collection, schema)
 export default User
