489c472bd585bbea79b7b007908c8b35d3b63285
Mat <matiastejerina94@gmail.com>
2023-04-21 18:02:13 -0300

    base de datos

--- models/Author.js
+++ models/Author.js
@@ -5,7 +5,7 @@ let schema = new Schema({
     last_name: {type:String, required:false},
     city:{type:String, required:true},
     country:{type:String, required:true},
-    date:{type:Date, required:true},
+    date:{type:Date},
     photo:{type:String, required:true},
     active:{type:Boolean, required:true},
     user_id:{type:Types.ObjectId, ref:'users', required:true}
@@ -13,5 +13,5 @@ let schema = new Schema({
     timestamps:true
 })
 let collection = 'authors'
-let Author = model(schema,collection)
+let Author = model(collection,schema)
 export default Author
