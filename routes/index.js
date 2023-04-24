5f435215bd7dba7f1a826b623bf52a8970ef833d
Mat <matiastejerina94@gmail.com>
2023-04-24 14:52:21 -0300

    B-08 y B-09

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import userRouter from './users.js'
+import authorRouter from './authors.js'
+import chaptersRouter from './chapters.js'
+import mangasRouter from './mangas.js'
+import categoriesRouter from './categories.js'
+import companiesRouter from './companies.js'
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',
+    subtitle: "algo" });
 });
+
+router.use('/auth', userRouter)
+router.use('/authors', authorRouter)
+router.use('/chapters', chaptersRouter)
+router.use('/mangas', mangasRouter)
+router.use('/categories', categoriesRouter)
+router.use('/companies', companiesRouter)
 
 export default router;
