bebe8aeed283d0d773b1f5a4128f9341c067d9e0
Claudio Pacheco <claudiopachecoramirez@gmail.com>
2023-05-04 12:37:31 -0400

    cambios finales back2

--- routes/chapters.js
+++ routes/chapters.js
@@ -2,13 +2,13 @@ import {Router} from 'express';
 
 import read from '../controllers/chapters/read.js';
 import create from '../controllers/chapters/create.js';
-import validator from '../middlewares/validator.js';
-import chapterExistsSingUp from "../middlewares/chapterSingUp.js"
+import validator2 from '../middlewares/validator2.js';
+import chapterExistsSingUp from "../middlewares/chapterSingUp.js";
 import { createChapterSchema } from '../schemas/chapters.js';
 
 let router = Router();
 
 router.get("/", read),
-router.post('/', validator(createChapterSchema), chapterExistsSingUp , create)
+router.post('/create', validator2(createChapterSchema), chapterExistsSingUp , create)
 
 export default router
