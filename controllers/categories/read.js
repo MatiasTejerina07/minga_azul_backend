342fb2af4e6235508a6e10e2dc875194d1c2f37e
Mat <matiastejerina94@gmail.com>
2023-05-09 19:09:29 -0300

    se añadio seguridad para datos sensibles

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -3,21 +3,21 @@ import Categories from "../../models/Category.js"
 
 
 
-let read = async(req, res, next)=>{
-    try{
+let read = async (req, res, next) => {
+    try {
         let all = await Categories.find()
         return res.status(200).json({
             categories: all
         })
-    }catch(error){
+    } catch (error) {
         return res.status(400).json({
             error: "error!"
         })
     }
-  
- }
 
- export default read
+}
+
+export default read
 
 
 
