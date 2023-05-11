3b7f61b9e3afc4422d8b1e3a20ef4e3d0ea95a74
Claudio Pacheco <claudiopachecoramirez@gmail.com>
2023-05-11 16:23:20 -0400

    correcciones de get one para poder pasar a los siguientes capitulos y datos del capitulo

--- controllers/chapters/get_one.js
+++ controllers/chapters/get_one.js
@@ -2,12 +2,19 @@ import Chapter from '../../models/Chapter.js'
 
 let get_one = async (req,res,next) => {
     try {
-        let { id } = req.params
-        let get_one = await Chapter.findOne({ _id:id }, 'pages title order -_id')
-        return res.status(200).json({
-            success: 'ok',
-            response: get_one
-        })
+        let id = req.params.id
+        let chapter = await Chapter.findById(id).select('title order pages manga_id -_id')
+        if(chapter){
+            let nextChapter = await Chapter.findOne({manga_id:chapter.manga_id, order:chapter.order+1})
+            return res.status(200).json({
+                success: 'ok',
+                chapter: chapter,
+                nextChapter:nextChapter? nextChapter._id:null
+            })
+        }
+            return res.status(404).json({
+                success: false
+            })
         } catch (error){
         next(error)
         }
