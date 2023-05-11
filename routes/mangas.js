959d54f8a039a3f17495d2af009d2c4ecbf08e0e
MatiasTejerina07 <127890622+MatiasTejerina07@users.noreply.github.com>
2023-05-11 19:51:28 -0300

    agrege pagination para llevarlo hacia el lado del front y poder trabajar con la data y la cantidad de pag que trae (#33)

--- routes/mangas.js
+++ routes/mangas.js
@@ -1,15 +1,16 @@
 import {Router} from "express";
-import read from "../controllers/mangas/read.js"
+import get_mangas from "../controllers/mangas/get_mangas.js";
 import passport from "../middlewares/passport.js";
 import validator from "../middlewares/validator.js";
 import create from '../controllers/mangas/create.js'
-import {mangasCreate} from '../schemas/mangas.js'
+import {mangasCreate} from '../schemas/mangas.js';
 import exists_title from "../middlewares/existsTitle.js";
-import find_id from '../middlewares/findsId.js'
-import get_one from '../controllers/mangas/get_one.js'
+import find_id from '../middlewares/findsId.js';
+import get_one from '../controllers/mangas/get_one.js';
+
 let router = Router()
 
-router.get('/', read)
+router.get('/', get_mangas)
 router.get('/:id', get_one)
 router.post('/',validator(mangasCreate),  passport.authenticate('jwt', {session: false}),exists_title,find_id ,create )
 
