de80b1e491f771cf76bb895810016e2bb347b06c
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-13 00:45:06 -0300

    destroy mangaAndChapters

--- routes/mangas.js
+++ routes/mangas.js
@@ -11,12 +11,14 @@ import get_me from '../controllers/mangas/get_me.js'
 import update from "../controllers/mangas/update.js";
 import is_active from "../middlewares/isactive.js"
 import is_property_of from "../middlewares/isPropertyOf.js"
+import destroy from "../controllers/mangas/destroy.js"
 let router = Router()
 
 router.get('/', get_mangas)
 router.get('/me', passport.authenticate('jwt', {session: false}),find_id, get_me)
 router.get('/:id', get_one)
 router.put('/:id',validator(mangasUpdate), passport.authenticate('jwt', {session: false}), find_id, is_active, is_property_of,  update)
+router.delete('/:id',validator(mangasUpdate), passport.authenticate('jwt', {session: false}), find_id, is_active, is_property_of,  destroy)
 router.post('/',validator(mangasCreate),  passport.authenticate('jwt', {session: false}),exists_title,find_id ,create )
 
 export default router
