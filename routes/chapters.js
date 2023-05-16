ca7094f9d9aedc15120e3d9e7976286c8fd4172a
Claudio Pacheco <claudiopachecoramirez@gmail.com>
2023-05-16 09:05:52 -0400

    configuracion endpoints editar chapter

--- routes/chapters.js
+++ routes/chapters.js
@@ -9,18 +9,25 @@ import existOrder from '../middlewares/existsorder.js';
 import isPropertyOf from '../middlewares/isPropertyOf.js';
 import nextOrder from '../middlewares/nextOrder.js';
 import mangaExists from '../middlewares/mangaExists.js';
-import find_id from '../middlewares/findsId.js'
+import find_id from '../middlewares/findsId.js';
+import isactive from '../middlewares/isactive.js';
 import addCoverPhoto from '../middlewares/addCoverPhoto.js';
 import get_one from '../controllers/chapters/get_one.js';
-import { createChapterSchema } from '../schemas/chapters.js';
-import getChapters from '../controllers/chapters/get_chapters.js'
+import get_me from '../controllers/chapters/get_me.js';
+import { createChapterSchema, updateChapterSchema } from '../schemas/chapters.js';
+import getChapters from '../controllers/chapters/get_chapters.js';
 import getPages from '../controllers/chapters/get_pages.js';
+import destroy from '../controllers/chapters/destroy.js';
+import update from '../controllers/chapters/update.js';
 
 let router = Router();
 
-router.get("/", getChapters);
+router.get('/', getChapters);
 router.get('/:id',get_one)
-router.get("/pages/:id", getPages)
+router.get('/pages/:id', getPages)
 router.post('/', validator(createChapterSchema),passport.authenticate("jwt", {session: false}),mangaExists, find_id ,isPropertyOf, chapterExists,addCoverPhoto ,nextOrder ,existOrder,  create)
+router.get('/me/:manga_id',passport.authenticate("jwt", {session: false}), find_id, get_me)
+router.put('/:id',validator(updateChapterSchema),passport.authenticate('jwt',{session:false}),/* find_id, isactive, isPropertyOf, */ update)
+router.delete('/:id',passport.authenticate('jwt',{session:false}), /* find_id, isactive, isPropertyOf , */ destroy)
 
 export default router
