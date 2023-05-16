5cd84d1cebcef8d07d544335d9a1d37ee849d501
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-05-16 12:06:14 -0300

    M 10 (#40)
    * get_me, updateManga, findsId fix, isPropertyOf fix, SchemaUpdateManga, routerManga
    
    * destroy mangaAndChapters
    
    * add title and category query for get_me
    
    * get_me

---
+++ controllers/mangas/destroy.js
@@ -0,0 +1,33 @@
+import Manga from "../../models/Manga.js";
+import Chapter from "../../models/Chapter.js"
+async function destroy(req, res, next){
+    try {
+        let manga = await Manga.findByIdAndDelete(req.params.id);
+        let chapters = await Chapter.deleteMany({manga_id: req.params.id})
+        if(manga.deletedCount || chapters.deletedCount){
+            return res.status(201).json({
+                success: true,
+                manga,
+                chapters
+            })
+        }
+        return res.status(404).json({
+            success: false,
+            message: [{
+                path: "exists",
+                message: "The manga doesnt exists"
+            }]
+        }) 
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: [{
+                path: "interal",
+                message: "internal server error"
+            }]
+        })
+    }
+    
+}
+
+export default destroy
